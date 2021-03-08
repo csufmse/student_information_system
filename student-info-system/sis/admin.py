5720dcc04f815b4976a7c132995f182156eddaf1
Bruce McKenzie <bjmckenz@gmail.com>
2021-03-07 23:56:34 -0800

    restyle

--- student-info-system/sis/admin.py
+++ student-info-system/sis/admin.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 
 from .models import Person, Semester
 
-class SemesterAdmin(admin.ModelAdmin):
-    list_display = ('name','date_registration_opens','date_started','date_last_drop','date_ended')
 
-admin.site.register(Semester,SemesterAdmin)
+class SemesterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_registration_opens', 'date_started',
+                    'date_last_drop', 'date_ended')
+
+
+admin.site.register(Semester, SemesterAdmin)
+
 
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ('name','email','is_admin','is_active','is_semester_admin','is_class_admin','is_grade_editor')
+    list_display = ('name', 'email', 'is_admin', 'is_active',
+                    'is_semester_admin', 'is_class_admin', 'is_grade_editor')
 
-admin.site.register(Person,PersonAdmin)
+
+admin.site.register(Person, PersonAdmin)
--- student-info-system/sis/models.py
+++ student-info-system/sis/models.py
@@ -2,22 +2,28 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from phone_field import PhoneField
 
+
 class Semester(models.Model):
-    name            = models.CharField('Semester Name',max_length=20, default='xxx')
+    name = models.CharField('Semester Name', max_length=20, default='xxx')
     date_registration_opens = models.DateField('Registration Opens')
-    date_started    = models.DateField('Classes Start')
-    date_last_drop  = models.DateField('Last Drop')
-    date_ended      = models.DateField('Classes End')
+    date_started = models.DateField('Classes Start')
+    date_last_drop = models.DateField('Last Drop')
+    date_ended = models.DateField('Classes End')
 
     def __str__(self):
         return self.name
 
+
 class Person(models.Model):
-    name = models.CharField('Name',max_length=128, help_text='Your Full Name')
-    address = models.CharField('Address',max_length=256, help_text='Your Mailing Address', blank=True)
-    email = models.EmailField(help_text='Your Primary Email Address', unique=True)
+    name = models.CharField('Name', max_length=128, help_text='Your Full Name')
+    address = models.CharField('Address',
+                               max_length=256,
+                               help_text='Your Mailing Address',
+                               blank=True)
+    email = models.EmailField(help_text='Your Primary Email Address',
+                              unique=True)
     phone = PhoneField(help_text='Your Primary Contact Phone', blank=True)
-    is_active = models.BooleanField('Is Active?',default=False)
+    is_active = models.BooleanField('Is Active?', default=False)
     is_admin = models.BooleanField('Is Admin?')
     is_semester_admin = models.BooleanField('Is Semester Admin?')
     is_class_admin = models.BooleanField('Is Class Admin?')
