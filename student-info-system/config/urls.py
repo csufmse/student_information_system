65d8fae26bf67c710209a50b87d5399dc9e2febd
Ocean Battista <oceanbattista@gmail.com>
2021-03-11 22:47:45 -0800

    Add studdent app

--- student-info-system/config/urls.py
+++ student-info-system/config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('sis/', include('sis.urls')),
+    path('student/', include('student.urls')),
     path("admin/", admin.site.urls),
 ]
