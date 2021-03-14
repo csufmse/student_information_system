cdf85843e22141c55326fd9dc87d94b27eab4ab7
Ocean Battista <oceanbattista@gmail.com>
2021-03-13 20:46:38 -0800

    add a way to get to current schedule

--- student-info-system/student/urls.py
+++ student-info-system/student/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 
 from . import views
 
+app_name = 'student'
 urlpatterns = [
     path('current_schedule', views.current_schedule_view, name='current_schedule'),
 ]
