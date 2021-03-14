142f645e0b4a1e1589424d872292c4ee72b4e67d
Ocean Battista <oceanbattista@gmail.com>
2021-03-13 20:18:00 -0800

    I swear this formatter >.<

--- student-info-system/student/views.py
+++ student-info-system/student/views.py
@@ -6,11 +6,11 @@ from django.contrib.auth.decorators import login_required
 def current_schedule_view(request):
     user = request.user
     if user.is_authenticated and not user.is_superuser and not user.is_staff:
-            context = {
-                'sections': user.student.sections.all,
-                'student_name': user.student.name,
-                'logged_in': True,
-            }
+        context = {
+             'sections': user.student.sections.all,
+             'student_name': user.student.name,
+             'logged_in': True,
+        }
     else:
         return redirect('/sis/access_denied')
     return render(request, 'student/current_schedule.html', context)
