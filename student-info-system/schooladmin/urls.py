0504355dccdef4773ce6d7cd58b172361b6da4df
Bruce McKenzie <bjmckenz@gmail.com>
2021-03-17 03:09:49 -0700

    deleting those losers

--- student-info-system/schooladmin/urls.py
+++ student-info-system/schooladmin/urls.py
@@ -6,5 +6,6 @@ app_name = 'schooladmin'
 urlpatterns = [
     path('', views.index, name='index'),
     path('users', views.users, name='users'),
+    path('user/<int:userid>', views.user, name='user'),
     path('new_user', views.new_user, name='new_user'),
 ]
