a7b5152f8734f8e33c31cb2f0f7a561002075387
Bruce McKenzie <bjmckenz@gmail.com>
2021-03-29 22:01:42 -0700

    better resizing and display of home and pages.

--- student-info-system/static/schooladmin/common.js
+++ student-info-system/static/schooladmin/common.js
@@ -3,7 +3,7 @@
 // Call this from JS at end.
 function addClickHandlers(template, row_class) {
     // Add onclick to each row so that it leaps off to the detail
-    for (const row of document.getElementsByClassName(row_class) {
+    for (const row of document.getElementsByClassName(row_class)) {
         var id = row.getAttribute('data-id');
         let url = template.replace(/1234599/, id.toString());
         row.addEventListener("click", function() {
@@ -50,5 +50,57 @@ function stylizeRange({field=0, name=0, min_label=0,
     e.style.width = width;
 }
 
+function pageY(elem) {
+    return elem.offsetParent ? (elem.offsetTop + pageY(elem.offsetParent)) : elem.offsetTop;
+}
+
+// for each table, the struct is <div dtalt><div table-structure><table><nav></div></div>
+// if you make the inner div scrollable, the pagination gets scrolled.
+// so we insert a new div:
+// <div dtalt><div tabstruct><nav stuff></div><div scrollable><table></div></div>
+// then the scrollable one can be...made scrollable
+function insertScrollableDivs(applyHandlers=false) {
+    for (const outerTableDiv of document.getElementsByClassName('scrollify-me')) {
+        const innerTable = outerTableDiv.getElementsByTagName("table")[0];
+        innerTable.remove();
+
+        const newScrollableDiv = document.createElement('div');
+        newScrollableDiv.className = "scrollable-div";
+        newScrollableDiv.append(innerTable);
+        outerTableDiv.insertBefore(newScrollableDiv, outerTableDiv.firstChild);
+    }
+
+    if (applyHandlers) {
+        var resizeableTableDiv;
+        const tableDivs = document.getElementsByClassName('expando-table');
+        if (tableDivs.length) {
+            resizeableTableDiv = tableDivs[0]
+        } else {
+            const scrollableDivs = document.getElementsByClassName('scrollable-div');
+            if (scrollableDivs.length) {
+                resizeableTableDiv = scrollableDivs[0]
+            }
+        }
+        if (resizeableTableDiv) {
+            const myDiv = resizeableTableDiv;
+            const innerTable = myDiv.getElementsByTagName("table")[0];
+            resize_function = function () {
+                const buffer = 25 + 50; // scroll bar buffer + nav button buffer
+                var height = document.documentElement.clientHeight;
+                height -= pageY(myDiv) + buffer;
+                height = (height < 0) ? 0 : height;
+                if (innerTable.offsetHeight < height) {
+                    myDiv.style.height = innerTable.offsetHeight + 'px';
+                } else {
+                    myDiv.style.height = height + 'px';
+                }
+            }
+            window.addEventListener('resize', resize_function);
+            // resize_function();
+            return resize_function;
+        }
+    }
+    return 0;
+}
 
 
