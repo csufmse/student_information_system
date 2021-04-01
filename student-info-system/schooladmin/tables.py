0b8f42c8b23d771989e6b16253cfd061d1ef2fcd
Bruce McKenzie <bjmckenz@gmail.com>
2021-04-01 11:26:12 -0700

    initial

--- student-info-system/schooladmin/tables.py
+++ student-info-system/schooladmin/tables.py
@@ -68,6 +68,32 @@ class FullUsersTable(tables.Table):
         attrs = {"class": 'fulluser_table'}
 
 
+class StudentInMajorTable(tables.Table):
+    username = ClassyColumn(css_class_base='username')
+    name = NameColumn(css_class_base='user_name')
+    is_active = AbilityColumn(null=False, attrs=field_css_classes('active'))
+    class_level = ClassyColumn(verbose_name='Class',
+                               css_class_base='classlevel',
+                               accessor='student__class_level')
+    gpa = ClassyColumn(verbose_name='GPA', css_class_base='gpa', accessor='student__gpa')
+
+    def render_gpa(self, value):
+        return '{:0.2f}'.format(value)
+
+    credits_earned = ClassyColumn(
+        verbose_name='Credits',
+        css_class_base='credits',
+        accessor='student__credits_earned',
+    )
+
+    class Meta:
+        model = User
+        template_name = "django_tables2/bootstrap.html"
+        fields = ('username', 'name', 'gpa', 'credits_earned', 'class_level')
+        row_attrs = {'class': 'user_row', 'data-id': lambda record: record.pk}
+        attrs = {"class": 'studentmajor_table'}
+
+
 class MajorsTable(tables.Table):
     abbreviation = ClassyColumn(css_class_base='major')
     name = ClassyColumn(css_class_base='majorname')
@@ -107,6 +133,20 @@ class CoursesTable(tables.Table):
         attrs = {"class": 'course_table'}
 
 
+# when the major is known
+class CoursesForMajorTable(tables.Table):
+    catalog_number = ClassyColumn(verbose_name='Catalog Number', css_class_base='catnumber')
+    title = ClassyColumn(css_class_base='coursetitle')
+    credits_earned = ClassyColumn(css_class_base='credits')
+
+    class Meta:
+        model = Course
+        template_name = "django_tables2/bootstrap.html"
+        fields = ('catalog_number', 'title', 'credits_earned')
+        row_attrs = {'class': 'course_row', 'data-id': lambda record: record.pk}
+        attrs = {"class": 'course_table'}
+
+
 class SemestersTable(tables.Table):
     semester = ClassyColumn(verbose_name='Session', css_class_base='semester')
     year = ClassyColumn(css_class_base='year')
@@ -121,6 +161,21 @@ class SemestersTable(tables.Table):
         template_name = "django_tables2/bootstrap.html"
         fields = ('semester', 'year', 'date_registration_opens', 'date_started', 'date_last_drop',
                   'date_ended')
+        attrs = {"class": 'semester_table'}
+        row_attrs = {'class': 'semester_row', 'data-id': lambda record: record.pk}
+
+
+class SemestersSummaryTable(tables.Table):
+    year = ClassyColumn(verbose_name='Year', css_class_base='year')
+    semester = ClassyColumn(verbose_name='Session', css_class_base='semester', accessor='name')
+
+    class Meta:
+        model = Semester
+        template_name = "django_tables2/bootstrap.html"
+        fields = (
+            'year',
+            'semester',
+        )
         attrs = {"class": 'semester_table'}
         row_attrs = {'class': 'semester_row', 'data-id': lambda record: record.pk}
 
@@ -145,7 +200,27 @@ class SectionsTable(tables.Table):
         attrs = {"class": 'section_table'}
 
 
+# for when Class is known
+class SectionForClassTable(tables.Table):
+    semester = ClassyColumn(css_class_base='sem')
+    status = ClassyColumn(css_class_base='sectionstatus')
+    hours = ClassyColumn(css_class_base='hours')
+    professor = ClassyColumn(css_class_base='user_name')
+    capacity = ClassyColumn(css_class_base='capac')
+    seats_remaining = ClassyColumn(css_class_base='remaining')
+
+    class Meta:
+        model = Section
+        template_name = "django_tables2/bootstrap.html"
+        fields = ('semester', 'status', 'hours', 'professor', 'capacity', 'seats_remaining')
+        row_attrs = {'class': 'section_row', 'data-id': lambda record: record.pk}
+        attrs = {"class": 'section_table'}
+
+
+# shows all info: semester, course, section, student
 class SectionStudentsTable(tables.Table):
+    semester = ClassyColumn(accessor='semester', css_class_base='semester')
+    course = ClassyColumn(accessor='course', css_class_base='course')
     username = ClassyColumn(accessor='student__user__username', css_class_base='username')
     name = ClassyColumn(accessor='student__user__name', css_class_base='user_name')
     major = ClassyColumn(accessor='student__major', css_class_base='major')
@@ -161,7 +236,68 @@ class SectionStudentsTable(tables.Table):
     class Meta:
         model = SectionStudent
         template_name = "django_tables2/bootstrap.html"
-        fields = ('username', 'name', 'major', 'status', 'letter_grade')
+        fields = ('semester', 'course', 'username', 'name', 'major', 'status', 'letter_grade')
+        row_attrs = {
+            'class': 'sectionstudent_row',
+            'data-id': lambda record: record.student.user.pk
+        }
+        attrs = {"class": 'sectionstudent_table'}
+
+
+# for when the class is known
+class StudentInSectionTable(tables.Table):
+    username = ClassyColumn(accessor='student__user__username', css_class_base='username')
+    name = ClassyColumn(accessor='student__user__name', css_class_base='user_name')
+    major = ClassyColumn(accessor='student__major', css_class_base='major')
+    gpa = ClassyColumn(accessor='student__gpa', css_class_base='gpa')
+
+    def render_gpa(self, value):
+        return '{:0.2f}'.format(value)
+
+    status = ClassyColumn(
+        verbose_name="Status",
+        css_class_base='sectionstatus',
+        accessor='status',
+    )
+    letter_grade = ClassyColumn(verbose_name="Grade",
+                                accessor='grade',
+                                css_class_base='lettergrade')
+
+    class Meta:
+        model = SectionStudent
+        template_name = "django_tables2/bootstrap.html"
+        fields = ('username', 'name', 'major', 'gpa', 'status', 'letter_grade')
+        row_attrs = {
+            'class': 'sectionstudent_row',
+            'data-id': lambda record: record.student.user.pk
+        }
+        attrs = {"class": 'sectionstudent_table'}
+
+
+# for when the student is known
+class StudentHistoryTable(tables.Table):
+    semester = ClassyColumn(accessor='section__semester', css_class_base='semester')
+    section = ClassyColumn(accessor='section', css_class_base='secnum')
+    course_title = ClassyColumn(accessor='section__course__title', css_class_base='coursetitle')
+    credits_earned = ClassyColumn(
+        verbose_name='Credits',
+        css_class_base='credits',
+        accessor='section__course__credits_earned',
+    )
+    status = ClassyColumn(
+        verbose_name="Status",
+        css_class_base='sectionstatus',
+        accessor='status',
+    )
+    letter_grade = ClassyColumn(verbose_name="Grade",
+                                accessor='grade',
+                                css_class_base='lettergrade')
+
+    class Meta:
+        model = SectionStudent
+        fields = ('semester', 'section', 'course_title', 'credits_earned', 'status',
+                  'letter_grade')
+        template_name = "django_tables2/bootstrap.html"
         row_attrs = {
             'class': 'sectionstudent_row',
             'data-id': lambda record: record.student.user.pk
