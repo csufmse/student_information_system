f31cb484a16dae42b0e601cc16b7904dc4a19498
Ocean Battista <oceanbattista@gmail.com>
2021-04-03 16:58:12 -0700

    tag works but not with django_tables2

--- student-info-system/sis/templatetags/sis-filter-tag.py
+++ student-info-system/sis/templatetags/sis-filter-tag.py
@@ -3,16 +3,15 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('sis_filter_tag.html')
-def sis_filter_tag(filt, has_filter, table, url, *args):
-    """Uses django-filter, django_tables2, and bootstrap. Requires a filter, filter values set,
-    table, the destination url for the page, and optional args for the destination url."""
+@register.inclusion_tag('sis/sis-filter-tag.html')
+def sis_filter_tag(filt, has_filter, destination):
+    """Uses django-filter and bootstrap. Requires a filter, has_filter bool, and 
+       the destination url for the page"""
 
     context = {
         'filt': filt,
         'has_filter': has_filter,
-        'table': table,
-        'dest': { 'url': url, 'args': [x for x in args] }
+        'destination': destination
     }
     return context
     
