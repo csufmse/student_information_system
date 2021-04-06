c9c4c9e6f3662e61460225dc45fe3185233043e7
Ocean Battista <oceanbattista@gmail.com>
2021-04-05 22:28:30 -0700

    add grades to section students view, add tests for new view

--- student-info-system/professor/views.py
+++ student-info-system/professor/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django_tables2 import RequestConfig
 
 from sis.authentication_helpers import role_login_required
-from sis.models import Professor, Section, AccessRoles, Semester
+from sis.models import Professor, Section, AccessRoles, Semester, SectionStudent
 from sis.utils import filtered_table
 
 from schooladmin.tables import SectionsTable, StudentInSectionTable
@@ -33,13 +33,16 @@ def sections(request):
 
 @role_login_required(AccessRoles.PROFESSOR_ROLE)
 def students_in_section(request, sectionid):
-    if request.POST:
-        request.POST.get
-    data = {'section': Section.objects.get(id=sectionid)}
-    data.update(
-        filtered_table(name='students',
-                       qs=Section.objects.get(id=sectionid).students.all(),
-                       filter=StudentFilter,
-                       table=StudentsTable,
-                       request=request))
+    data = {}
+    section = Section.objects.get(id=sectionid)
+    ssects = SectionStudent.objects.filter(section=section)
+    if request.method == "POST":
+        for student in section.students.all():
+            if request.POST.get(str(student)):
+                ssect = ssects.get(student=student)
+                ssect.grade = request.POST.get(str(student))
+                ssect.save()
+                data['grade_submitted'] = {True}
+
+    data.update({'grades': SectionStudent.GRADES, 'section': section, 'ssects': ssects})
     return render(request, 'professor/students.html', data)
