2a894ff76766818cdb6d8d376e385b68973c7b5f
Ocean Battista <48607147+oceancypress@users.noreply.github.com>
2021-04-13 17:19:25 -0700

    accidentally pushed old change

--- student-info-system/schooladmin/apps.py
+++ student-info-system/schooladmin/apps.py
@@ -3,8 +3,3 @@ from django.apps import AppConfig
 
 class SchooladminConfig(AppConfig):
     name = 'schooladmin'
-    label = 'admin.school'
-
-    def ready(self):
-        from . import scheduler
-        scheduler.start()
