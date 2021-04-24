858a29b339fc912df9289f9940b8d21c574bb980
Bruce McKenzie <bjmckenz@gmail.com>
2021-04-24 00:17:38 -0700

    #379 add semester selector for books

--- student-info-system/student/views.py
+++ student-info-system/student/views.py
@@ -219,11 +219,18 @@ def drop(request, id):
 @role_login_required(Profile.ACCESS_STUDENT)
 def secitems(request):
     the_user = request.user
+
     the_semester = Semester.current_semester()
+    if request.method == 'POST':
+        the_id = request.POST.get('semester', the_semester.id)
+        the_semester = Semester.objects.get(id=the_id)
+
     data = {
         'auser': the_user,
         'semester': the_semester,
+        'semesters': the_user.profile.student.semesters.all(),
     }
+
     data.update(
         filtered_table2(
             name='secitems',
